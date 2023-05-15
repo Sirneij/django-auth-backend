bf8c6cb1bd584b187911a9e44d70f3d3c1c9b9b9
John Idogun <sirneij@gmail.com>
2023-05-15 13:53:36 +0100

    Static analysis done

--- src/users/models.py
+++ src/users/models.py
@@ -3,6 +3,8 @@ from typing import Any
 
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class UserManager(BaseUserManager):  # type:ignore
@@ -65,3 +67,11 @@ class UserProfile(models.Model):
         """Return a string representation of this UserProfile."""
         string = self.user.email if self.user.email != '' else self.user.get_full_name()
         return f'<UserProfile {self.id} {string}>'
+
+
+@receiver(post_save, sender=User)
+def update_user_profile_signal(sender: Any, instance: User, created: bool, **kwargs: dict[str, Any]) -> None:
+    """Create or update UserProfile model after each user gets created or updated."""
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
