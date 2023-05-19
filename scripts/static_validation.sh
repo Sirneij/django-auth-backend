9133658798ca5b57df33b825aa518ac75b99a879
John Idogun <sirneij@gmail.com>
2023-05-19 21:18:34 +0100

    Some changes

--- scripts/static_validation.sh
+++ scripts/static_validation.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
+
+# checks whether or not the source files conform with black and isort formatting
 black --skip-string-normalization --line-length 120 --check tests
 black --skip-string-normalization --line-length 120 --check src
-
 isort --atomic --profile black -c src
 isort --atomic --profile black -c tests
 
-cd src || exit 1
+cd src
 
+# Exits with non-zero code if there is any model without a corresponding migration file
 python manage.py makemigrations --check --dry-run
 
+# Uses propector to ensure that the source code conforms with Python best practices
 prospector  --profile=../.prospector.yml --path=. --ignore-patterns=static
 
+# Analysis and checks whether or not we have common security issues in our Python code. 
 bandit -r .
 
+# Checks for correct annotations
 mypy .
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-py.test -n 4 --nomigrations --reuse-db -W error::RuntimeWarning --cov=src --cov-report=html tests/
+py.test -n auto --nomigrations --reuse-db -W error::RuntimeWarning --cov=src --cov-report=html tests/
